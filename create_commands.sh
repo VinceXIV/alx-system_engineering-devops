8d594712158b8ab3eec9ce495084e7bacb9aece3
Makokha Omondi <makokhavomondi@gmail.com>
2022-06-22 03:47:16 -0700

    add the file; $ARG1

--- create_commands.sh
+++ create_commands.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ARG1=${1:-script_name}
-ARG2=${3:-command}
+set /p script_name = script_name
+set /p script_command = script_command
 
-touch 0x00-shell_basics/$ARG1
-echo '#!/bin/bash' > 0x00-shell_basics/$ARG1
-echo $ARG2 > 0x00-shell_basics/$ARG1
+touch 0x00-shell_basics/script_name
+echo '#!/bin/bash' > 0x00-shell_basics/script_name
+echo script_command > 0x00-shell_basics/script_name
 
-chmod u+x 0x00-shell_basics/$ARG1
+chmod u+x 0x00-shell_basics/script_name
 
 git add .
 git commit -m 'add the file; $ARG1'
